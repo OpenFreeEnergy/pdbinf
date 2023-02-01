34a306c191b8042a3e155068396cfd7e35c53a52
richard <richardjgowers@gmail.com>
2023-02-01 10:35:24 +0000

    added license headers

--- src/pdbinf/__init__.py
+++ src/pdbinf/__init__.py
@@ -1,4 +1,7 @@
+# This code is part of pdbinf and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/pdbinf
 import logging
+
 from ._standard_AAs import STANDARD_AA_DOC
 from . import _pdbinf
 from ._pdbinf import (
--- src/pdbinf/_standard_AAs.py
+++ src/pdbinf/_standard_AAs.py
@@ -1,3 +1,5 @@
+# This code is part of pdbinf and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/pdbinf
 import gemmi
 
 
