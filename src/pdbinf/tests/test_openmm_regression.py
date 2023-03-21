0744877f18a6a84255e40304a6499a5deabdcc31
richard gowers <richardjgowers@gmail.com>
2023-03-21 15:56:17 +0000

    add another openmm regression test

--- src/pdbinf/tests/test_openmm_regression.py
+++ src/pdbinf/tests/test_openmm_regression.py
@@ -38,7 +38,22 @@ def plb_proteins(request):
     return PDBS.fetch('{}/01_protein/crd/protein.pdb'.format(request.param))
 
 
-def test_openmm_regression(plb_proteins, tpo_doc, request):
+def assert_bonds_match(ref, other):
+    # check that all bonds in ref (an openmm PDBFile) are in other (an rdkit mol)
+    i = 0
+    for b in ref.topology.bonds():
+        ix1, ix2 = b.atom1.index, b.atom2.index
+
+        x = other.GetBondBetweenAtoms(ix1, ix2)
+
+        assert x is not None, (ix1, ix2)
+        i += 1
+
+    # checks that other cannot contain any additional bonds
+    assert i == other.GetNumBonds()
+
+
+def test_openmm_regression(plb_proteins, tpo_doc):
     ref = app.PDBFile(plb_proteins)
 
     if 'cdk2' in plb_proteins:
@@ -48,9 +63,12 @@ def test_openmm_regression(plb_proteins, tpo_doc, request):
 
     m = pdbinf.load_pdb_file(plb_proteins, templates=t)
 
-    for b in ref.topology.bonds():
-        ix1, ix2 = b.atom1.index, b.atom2.index
+    assert_bonds_match(ref, m)
 
-        x = m.GetBondBetweenAtoms(ix1, ix2)
 
-        assert x is not None
+def test_nucleic_regression(openmm_nucleic):
+    ref = app.PDBFile(openmm_nucleic)
+
+    m = pdbinf.load_pdb_file(openmm_nucleic, templates=[pdbinf.RNA_DOC, pdbinf.DNA_DOC])
+
+    assert_bonds_match(ref, m)
